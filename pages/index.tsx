5d1b67dcc027d1b78610961f8408832334e120a7
renato76 <renmignogna@gmail.com>
2021-10-14 20:02:58 +0100

    create startup files - add typescript

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  const message: string = "Hello World"
+  let a: AddEventListenerOptions
+  let b: NodeJS.Process
+
+  let person: Person = {
+    name: "Pedro"
+  }
+
+  return (
+    <div>
+      {message}
+    </div>
+  )
+}
