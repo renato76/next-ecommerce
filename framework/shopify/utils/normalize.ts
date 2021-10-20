28bec2754ff2e05a0c62de0b8d566b381e3f02a2
renato76 <renmignogna@gmail.com>
2021-10-20 23:41:12 +0100

    normalize product and images

---
+++ framework/shopify/utils/normalize.ts
@@ -0,0 +1,34 @@
+import { 
+  ImageEdge,
+  Product as ShopifyProduct
+} from "../schema"
+
+const normalizeProductImages = ({edges}: {edges: ImageEdge[]}) => 
+  edges.map(({node: {originalSrc: url, ...rest}}) => ({
+        url: `/images/${url}`,
+        ...rest }
+  ))
+
+export function normalizeProduct(productNode: ShopifyProduct): any {
+  const {
+    id, 
+    title: name,
+    handle,
+    vendor,
+    description,
+    images: imageConnection,
+    ...rest
+  } = productNode
+
+  const product = {
+    id, 
+    name, 
+    vendor,
+    description,
+    path: `/${handle}`,
+    slug: handle.replace(/^\/+|\/+$/g, ""),
+    images: normalizeProductImages(imageConnection),
+    ...rest 
+  }
+  return product
+}
