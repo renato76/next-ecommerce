09b8c7a1d12bc7fb012c0d8e4910b4a8812cc772
renato76 <renmignogna@gmail.com>
2021-10-21 16:30:01 +0100

    format json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const { withFrameworkConfig } = require("./framework/common/config")
 
 module.exports = withFrameworkConfig({
+  framework: {
+    name: "shopify"
+  },
   reactStrictMode: true,
   i18n: {
     locales: ["en-US", "es"],
