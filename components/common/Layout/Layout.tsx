f1243041cc2b3c14159883a956a2382b3178e631
renato76 <renmignogna@gmail.com>
2021-11-09 22:23:19 +0000

    create navbar

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,12 +1,15 @@
 import { FC } from 'react'
-import style from "./Layout.module.css"
+import s from "./Layout.module.css"
+import { Footer, Navbar } from "@components/common"
 
 const Layout: FC = ({ children }) => {
   return (
-    <div className={style.root}>
+    <div className={s.root}>
+      <Navbar />
       <main className="fit">
         {children}
       </main>
+      <Footer />
     </div>
   )
 }
---
+++ components/common/Navbar/Navbar.tsx
@@ -0,0 +1,37 @@
+import { FC } from "react"
+import { Container } from "@components/ui"
+import Link from "next/link"
+import s from "./Navbar.module.css"
+
+const Navbar: FC = () => {
+
+  return (
+    <Container>
+      <div className={s.root}>
+        <div className="flex flex-1 items-center">
+          <Link href="/">
+            <a className={s.logo}>
+              NEXT STORE
+            </a>       
+          </Link>
+          <nav className="ml-6 space-x-6">
+            <Link href="/">
+              <a className={s.link}>ALL</a>
+            </Link>
+            <Link href="/">
+              <a className={s.link}>Clothes</a>
+            </Link>
+            <Link href="/">
+              <a className={s.link}>Accessories</a>
+            </Link>
+            <Link href="/">
+              <a className={s.link}>Shoes</a>
+            </Link>
+          </nav>
+        </div>
+      </div>
+    </Container>
+  )
+}
+
+export default Navbar
