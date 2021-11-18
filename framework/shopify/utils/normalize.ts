99190ba376924f63734e5d7b76c5ae2c509bd758
renato76 <renmignogna@gmail.com>
2021-11-18 21:03:01 +0000

    normalize options

--- framework/shopify/utils/normalize.ts
+++ framework/shopify/utils/normalize.ts
@@ -1,44 +1,64 @@
-import { 
+
+import {
   ImageEdge,
   MoneyV2,
-  Product as ShopifyProduct
+  Product as ShopifyProduct,
+  ProductOption
 } from "../schema"
 
 import { Product } from "@common/types/product"
 
-const normalizeProductImages = ({edges}: {edges: ImageEdge[]}) => 
-  edges.map(({node: {originalSrc: url, ...rest}}) => ({
-        url: `/images/${url}`,
-        ...rest }
+const normalizeProductImages = ({edges}: {edges: Array<ImageEdge>}) =>
+  edges.map(({node: { originalSrc: url, ...rest}}) => ({
+      url: `/images/${url}`,
+      ...rest }
   ))
 
-  const normalizeProductPrice = ({currencyCode, amount}: MoneyV2) => ({ 
-    value: +amount, 
-    currencyCode
-  })
+const normalizeProductPrice = ({currencyCode, amount}: MoneyV2) => ({
+  value: +amount,
+  currencyCode
+})
+
+const normalizeProductOption = ({
+  id,
+  values,
+  name: displayName
+}: ProductOption) => {
+
+  console.log("ID ", id)
+  console.log("NAME ", displayName)
+  console.log("VALUES ", values)
+
+  return {}
+}
 
 export function normalizeProduct(productNode: ShopifyProduct): Product {
   const {
-    id, 
+    id,
     title: name,
     handle,
     vendor,
     description,
     images: imageConnection,
     priceRange,
+    options,
     ...rest
   } = productNode
 
   const product = {
-    id, 
-    name, 
+    id,
+    name,
     vendor,
     description,
     path: `/${handle}`,
     slug: handle.replace(/^\/+|\/+$/g, ""),
     images: normalizeProductImages(imageConnection),
-    price: normalizeProductPrice(priceRange.minVariantPrice), 
-    ...rest 
+    price: normalizeProductPrice(priceRange.minVariantPrice),
+    options: options ?
+      options.filter(o => o.name !== "Title")
+            .map(o => normalizeProductOption(o)) : [],
+    ...rest
   }
+
   return product
 }
